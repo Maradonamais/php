95b5d67b900cbf51223c7d52ad08c74321529ad2
Vinicius Oliveira <ViniciusOliveira221@outlook.com>
2018-11-29 14:10:43 -0200

    arrumei o conflito de envio de modificacao

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 echo "Olá mundo 456";
 
-echo "olá udemy"
-=======
-echo "Olá mundo";
-echo "Olá Hcode"
->>>>>>> 7fa83690ce637ca14f5163b7b07a3b9df97cf62a
+echo "olá udemy e hcode"
+
 ?>
